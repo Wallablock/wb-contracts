cc1621a7ffcf69220260d582c8cc54be814a9661
Victor Guardia <victor.guardia@est.fib.upc.edu>
2020-05-05 11:59:36 +0200

    Remove OfferFactory contract as it is no longer needed by frontend

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,5 @@
 const Migrations = artifacts.require("Migrations");
-const OfferFactory = artifacts.require("OfferFactory");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(OfferFactory);
 };
